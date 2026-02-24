92660c579e384a032fe91dbb6d867eb62fbe2156
qsum99 <someshwarkumbhar87@gmail.com>
2026-02-24 23:09:31 +0530

    phase 2 api.py updated

--- api.py
+++ api.py
@@ -1,24 +1,19 @@
 import requests 
 city_name="New Delhi"
 api="1e5ef983a10f2fd31654af73150a176a"
-url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api}"
+url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api}&units=metric"
 
 response=requests.get(url)
 
 if response.status_code == 200:
     data=response.json()
-    lon=data["coord"]["lon"]
-    lat=data["coord"]["lat"]
-url_for_rainfall=f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}"
-
-response2=requests.get(url_for_rainfall)
-
-if response2.status_code == 200:
-    data2=response2.json()
+    temp=data["main"]["temp"]
+    humidity=data["main"]["humidity"]
     rain_data = data.get("rain", {})
 
     rain_last_hour = rain_data.get("1h", 0)
-    
+    print(f"Temperature: {temp}")
+    print(f"Humidity: {humidity}")
     print(f"Rain in the last hour: {rain_last_hour} mm")
 
 
